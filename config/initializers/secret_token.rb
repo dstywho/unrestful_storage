d45e1cb8d3a635c65a313238b21a8012d4bf5b1d
dustin <dstywho@gmail.com>
2011-09-06 22:47:49 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnrestfulStorage::Application.config.secret_token = 'e797735666f693d908f092394e6a0cd73375dfcdd6d4f5015863ff790a36010df853d4f68f57f9f86cb2a1a692b00c912a1625bea4f7a37d57fac9af1476e824'
